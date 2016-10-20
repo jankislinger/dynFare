17a09df401caac0e08d6c7cb8a5fbad7561599ed
Jan Kislinger <jan.kislinger@gmail.com>
2016-10-20 14:30:49 +0200

    rename package

---
+++ R/dynFare-package.R
@@ -0,0 +1,12 @@
+#' dynFare: models for dynamic fare price optimization
+#'
+#' Description
+#'
+#' @docType package
+#' @name dynFare
+#'
+#' @importFrom dplyr 
+#' @importFrom foreach foreach %do%
+#' @importFrom magrittr %>% %$% %T>% %<>% set_colnames
+
+NULL
