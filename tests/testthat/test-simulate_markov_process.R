1ddb5bd894f0b6553445d396cb0e61d689923780
Jan Kislinger <jan.kislinger@gmail.com>
2016-10-20 14:29:19 +0200

    update tests

---
+++ tests/testthat/test-simulate_markov_process.R
@@ -0,0 +1,19 @@
+library(dynFare)
+context("Simulate Markov process")
+
+test_that('standard return', {
+  
+  routes         <- get_routes(4, 60, c(0.3, 0.4, 0.5))
+  beta           <- matrix(rep(c(18,-4,4, 1.2), each = 6), nrow = 6)
+  price          <- rep(250, 6)
+  init_occupancy <- rep(0, 3)
+  
+  x <- simulate_markov_process(model = 1, routes, beta, price = price,
+                               from = 0, to = 1, nseats = 10, init_occupancy)
+  
+  expect_type(x, 'list')
+  expect_named(x, c('tickets', 'occupancy', 'reward'))
+  expect_type(x$tickets, 'double')
+  expect_type(x$occupancy, 'double')
+  expect_type(x$reward, 'double')
+})
--- tests/testthat/test-simulate_poisson_process.R
+++ tests/testthat/test-simulate_poisson_process.R
@@ -1,7 +1,8 @@
-library(dynamicFare)
+library(dynFare)
 context("Simulate Poisson process")
 
 test_that('standard return', {
-  x <- simulate_poisson_process(1, rep(0.1, 4), 150, 0, 1)
+  x <- simulate_poisson_process(model = 1, beta = c(18,-4, 4, 1.2), price = 250,
+                                from = 0, to = 1)
   expect_type(x, 'double')
 })
