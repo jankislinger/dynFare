f5a6d1d81db2665eebdb732d81a317d9668e7943
Jan Kislinger <jan.kislinger@gmail.com>
2016-10-20 14:30:22 +0200

    markov process: add documentation, code cleaning, renaming

--- R/simulate_markov_process.R
+++ R/simulate_markov_process.R
@@ -1,4 +1,41 @@
-SimulateMarkovProcess <- function(model, routes, beta, price, from, to, nseats, init.occupancy) {
+#' Simulate demand for whole train
+#' 
+#' @description
+#' Simulates demand for whole train over given time interval as an inhomogeneous
+#' Markov process (combination of multiple Poisson processes).
+#' 
+#' @param model Model selection; either 1 or 2. See details.
+#' @param routes Numeric matrix containing information about the routes. Each row represents one route.
+#'               The order of routes needs to be the same in all arguments. See examples.
+#' @param beta Numeric matrix of model parameters. Each row contains parameters for one route.
+#'             The number of columns corresponds with the model selection. See details.
+#' @param price Vector of ticket prices. Each element represents one route.
+#' @param from Lower bound of the time interval over which the process should be simulated.
+#' @param to Upper bound of the time interval over which the process should be simulated.
+#' @param nseat Number of seats in the train.
+#' @param init_occupancy Occupancy of the train at time \code{from}. Represents state of the Markov process.
+#' 
+#' @details 
+#' 	The models for intensity function are described in documentation for function \link{SimulatePoissonProcess}.
+#' 	
+#' @return 
+#' 	\describe{
+#' 	  \item{tickets}{Matrix containing information about purchased tickets during given time interval.}
+#' 	  \item{occupancy}{Occupancy of the train at time \code{to}.}
+#' 	  \item{reward}{Cumulative reward of the transitions. Sum of prices of sold tickets.}
+#' 	}
+#' 
+#' @examples 
+#' routes         <- get_routes(4, 60, c(0.3, 0.4, 0.5))
+#' beta           <- matrix(rep(c(18,-4,4, 1.2), each = 6), nrow = 6)
+#' price          <- rep(250, 6)
+#' init_occupancy <- rep(0, 3)
+#' 
+#' simulate_markov_process(model = 1, routes, beta, price = price,
+#'                         from = 0, to = 1, nseats = 10, init_occupancy)
+
+
+simulate_markov_process <- function(model, routes, beta, price, from, to, nseats, init_occupancy) {
 
   ## set maximal from and to values for time
   from <- pmin(from, routes[,'dep_daytime'])
@@ -7,18 +44,18 @@ SimulateMarkovProcess <- function(model, routes, beta, price, from, to, nseats, 
   ## simulate Poisson process for each route
   tickets <- foreach(i = 1:nrow(routes), .combine = rbind) %do% {
 
-    pois <- SimulatePoissonProcess(model, beta[i,], price[i], from[i], to[i])
+    pois <- simulate_poisson_process(model, beta[i,], price[i], from[i], to[i])
 
     if (length(pois) > 0) {
       cbind(route = i, time = pois, count = 1)
     } else {
-      matrix(0, 0, 3) %>% setColnames(c('route', 'time', 'count'))
+      matrix(0, 0, 3, dimnames = list(NULL, c('route', 'time', 'count')))
     }
   }
 
   ## return empty if no passenger
   if(nrow(tickets) == 0) {
-    return(list(tickets = NULL, occupancy = init.occupancy, reward = 0))
+    return(list(tickets = NULL, occupancy = init_occupancy, reward = 0))
   }
 
   ## order passangers by purchase time
@@ -26,15 +63,15 @@ SimulateMarkovProcess <- function(model, routes, beta, price, from, to, nseats, 
 
   ## bind route information to each ticket
   tickets %<>% cbind(routes[tickets[,'route'], c('from', 'to'), drop = F])
-  tickets <<-tickets; init.occupancy <<- init.occupancy
+
   ## calculate cumulated occupancy
   occupancy <- routes[tickets[, 'route'], grepl('^occup.*', colnames(routes)), drop = F] %>%
     sweep(1, tickets[,'count'], '*') %>%
     apply(2, cumsum) %>%
     KeepAsMatrix() %>%
-    sweep(2, init.occupancy, '+')
+    sweep(2, init_occupancy, '+')
 
-  out <- DropExceeding(tickets, occupancy, nseats)
+  out <- drop_exceeding(tickets, occupancy, nseats)
   out$reward <- price[out$tickets[,'route']] %*% out$tickets[,'count'] %>% drop
   return(out)
 
@@ -43,7 +80,7 @@ SimulateMarkovProcess <- function(model, routes, beta, price, from, to, nseats, 
 
 # drop exceeding passengers -----------------------------------------------
 
-DropExceeding <- function(tickets, occupancy, nseats) {
+drop_exceeding <- function(tickets, occupancy, nseats) {
 
   ## if the train did not exceed its capacity return all tickets
   if (nrow(occupancy) == 0 || max(occupancy) <= nseats) {
@@ -62,13 +99,5 @@ DropExceeding <- function(tickets, occupancy, nseats) {
     tickets[which.exceed, 'to'] < tickets[(which.exceed + 1):nrow(tickets), 'from']) %>%
     which %>% c(0) + which.exceed
 
-  DropExceeding(tickets[-which.drop,, drop = F], occupancy[-which.drop,, drop = F], nseats)
-}
-
-printDim <- function(x) {
-  if (x %>% dim %>% is.null) {
-    x %>% length %>% print
-  } else {
-    x %>% dim %>% print
-  }
+  drop_exceeding(tickets[-which.drop,, drop = F], occupancy[-which.drop,, drop = F], nseats)
 }
