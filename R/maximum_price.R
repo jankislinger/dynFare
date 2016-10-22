df17c8c56d00105c8a8ce408ebdbd218f9da2343
Jan Kislinger <jan.kislinger@gmail.com>
2016-10-22 12:49:47 +0200

    function maximum_price (dummy)

---
+++ R/maximum_price.R
@@ -0,0 +1,15 @@
+#' Maximum acceptable price for passangers
+#' 
+#' Simulate maximum price that a passanger is available to pay for the ticket.
+#' The distribution of maximum price is conditional on current price - only passengers that would buy the ticket for \code{price}.
+#' 
+#' @param beta Parameters of the Poisson process
+#' @param price Actual price of the tickets.
+#' @param time Vector of times of purchases for individual passengers
+#' 
+#' @export
+
+maximum_price <- function(beta, price, time) {
+  
+  price + runif(length(time), max = 50)
+}
