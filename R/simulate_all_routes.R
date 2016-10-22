b79839b7335558aedbbbddc9794e818c30c55926
Jan Kislinger <jan.kislinger@gmail.com>
2016-10-22 15:12:08 +0200

    add keep_all argument; documentation for drop_exceeding()

--- R/simulate_all_routes.R
+++ R/simulate_all_routes.R
@@ -13,6 +13,7 @@
 #' @param to Upper bound of the time interval over which the process should be simulated.
 #' @param nseat Number of seats in the train.
 #' @param init_occupancy Occupancy of the train at time \code{from}. Represents state of the Markov process.
+#' @param keep_all Logical; Should the passengers that exceed the capacity be returned as well?
 #' 
 #' @details 
 #' 	The models for intensity function are described in documentation for function \link{SimulatePoissonProcess}.
@@ -36,7 +37,7 @@
 #' @export
 
 
-simulate_all_routes <- function(routes, model, beta, price, from, to, nseats, init_occupancy) {
+simulate_all_routes <- function(routes, model, beta, price, from, to, nseats, init_occupancy, keep_all) {
 
   ## set maximal from and to values for time
   from <- pmin(from, routes[,'dep_daytime'])
@@ -47,10 +48,17 @@ simulate_all_routes <- function(routes, model, beta, price, from, to, nseats, in
 
     pois <- simulate_one_route(model, beta[i,], price[i], from[i], to[i])
 
-    if (length(pois) > 0) {
-      cbind(route = i, time = pois, count = 1)
+    if (length(pois) > 0 && keep_all) {
+      cbind(route = i, time = pois, count = 1, price = price[i]) %>% 
+        as.data.frame() %>% 
+        mutate(max_price = maximum_price(beta[i,], price, time)) %>%
+        as.matrix()
+    } else if (length(pois) > 0) {
+      cbind(route = i, time = pois, count = 1, price = price[i])
+    } else if (keep_all) {
+      matrix(0, 0, 5, dimnames = list(NULL, c('route', 'time', 'count', 'price', 'max_price')))
     } else {
-      matrix(0, 0, 3, dimnames = list(NULL, c('route', 'time', 'count')))
+      matrix(0, 0, 4, dimnames = list(NULL, c('route', 'time', 'count', 'price')))
     }
   }
 
@@ -72,20 +80,32 @@ simulate_all_routes <- function(routes, model, beta, price, from, to, nseats, in
     KeepAsMatrix() %>%
     sweep(2, init_occupancy, '+')
 
-  out <- drop_exceeding(tickets, occupancy, nseats)
-  out$reward <- price[out$tickets[,'route']] %*% out$tickets[,'count'] %>% drop
+  out <- if (keep_all) {
+    list(tickets = tickets, occupancy = occupancy[nrow(occupancy),])
+  } else {
+    drop_exceeding(tickets, occupancy, nseats)
+  }
+  out$reward <- price[out$tickets[,'route']] %*% out$tickets[,'count'] %>% drop()
   return(out)
-
 }
 
 
-# drop exceeding passengers -----------------------------------------------
+#' Drop exceeding passengers
+#' 
+#' Loops iteratively through all tickets and excludes passengers that do not fit the cappacity of a train.
+#' 
+#' @param tickets Matrix containing information about the tickets.
+#' @param occupancy Matrix of train occupancy after a passanger buys a ticket.
+#'                  Rows are related to passengers, columns to pairs of consecutive stations.
+#' @param nseat Number of seats in the train.
+#' 
+#' @export
 
 drop_exceeding <- function(tickets, occupancy, nseats) {
 
   ## if the train did not exceed its capacity return all tickets
   if (nrow(occupancy) == 0 || max(occupancy) <= nseats) {
-    return(list(tickets = tickets, occupancy = occupancy[nrow(occupancy),,drop = F]))
+    return(list(tickets = tickets, occupancy = occupancy[nrow(occupancy),]))
   }
 
   ## find the first passenger (ticket) that exceeds the capacity
