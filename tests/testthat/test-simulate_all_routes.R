8d5d7096a85618b316e94d8fa0f9bed9b311a661
Jan Kislinger <jan.kislinger@gmail.com>
2016-10-22 15:12:22 +0200

    add keep_all to tests

--- tests/testthat/test-simulate_all_routes.R
+++ tests/testthat/test-simulate_all_routes.R
@@ -1,5 +1,5 @@
 library(dynFare)
-context("Simulate Markov process")
+context("Simulate all routes")
 
 test_that('standard return', {
   
@@ -8,8 +8,7 @@ test_that('standard return', {
   price          <- rep(250, 6)
   init_occupancy <- rep(0, 3)
   
-  x <- simulate_all_routes(model = 1, routes, beta, price = price,
-                           from = 0, to = 1, nseats = 10, init_occupancy)
+  expect_silent(x <- simulate_all_routes(routes, 1, beta, price, 0, 1, 10, init_occupancy, F))
   
   expect_type(x, 'list')
   expect_named(x, c('tickets', 'occupancy', 'reward'))
--- tests/testthat/test-simulate_one_route.R
+++ tests/testthat/test-simulate_one_route.R
@@ -2,7 +2,9 @@ library(dynFare)
 context("Simulate Poisson process")
 
 test_that('standard return', {
-  x <- simulate_one_route(model = 1, beta = c(18,-4, 4, 1.2), price = 250,
-                                from = 0, to = 1)
+  
+  beta <- c(18,-4, 4, 1.2)
+  
+  expect_silent(x <- simulate_one_route(1, beta, 250, 0, 1))
   expect_type(x, 'double')
 })
